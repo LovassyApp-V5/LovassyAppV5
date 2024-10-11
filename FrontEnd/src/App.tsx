c0c8336cc2d755071e4949437824d2011584b244
Cs0ng0r <bikadicsongor@gmail.com>
2024-10-11 10:47:10 +0200

    remove:navbar

--- FrontEnd/src/App.tsx
+++ FrontEnd/src/App.tsx
@@ -2,13 +2,13 @@ import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 import { theme } from "./theme";
 import { Welcome } from "./Welcome/Welcome";
-import { ColorSchemeToggle } from "./ColorSchemeToggle/ColorSchemeToggle";
+import { Navbar } from "./components/Navbar";
 
 export default function App() {
   return (
     <MantineProvider theme={theme}>
+      <Navbar />
       <Welcome />
-      <ColorSchemeToggle />
     </MantineProvider>
   );
 }
